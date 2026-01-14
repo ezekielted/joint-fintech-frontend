104175a206a04a77ca567e1b1608c49724ab7cd8
Ezekiel <temiforbes@gmail.com>
2026-01-14 09:42:27 +0100

    Set default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem={true}
           disableTransitionOnChange
         >
